d8daf7de0763ae5adc54d0728a595c5c6dc77568
Vuk Radovic <vuk119@gmail.com>
2022-08-23 22:16:39 +0100

    p3

--- 3.py
+++ 3.py
@@ -16,8 +16,52 @@ def solve1(numbers) -> int:
 
     return sum(gamma) * sum(epsilon)
 
+def step(numbers, flag, i) -> int:
+    sum = 0
+
+    for number in numbers:
+        if number[i] == '0':
+            sum -= 1
+        elif number[i] == '1':
+            sum += 1
+
+    if flag:
+        if sum >= 0:
+            target = '1'
+        else:
+            target = '0'
+    else:
+        if sum >= 0:
+            target = '0'
+        else:
+            target = '1'
+
+    return [n for n in numbers if n[i]==target]
+
+
+def solve2(numbers) -> int:
+
+    oxygen = numbers
+    i = 0
+    while len(oxygen) > 1:
+        oxygen = step(oxygen, True, i)
+        i += 1
+    oxygen = oxygen[0]
+    oxygen_rating = [int(oxygen[i]) * 2 ** (11 - i) for i in range(12)]
+
+    co2 = numbers
+    i = 0
+    while len(co2) > 1:
+        co2 = step(co2, False, i)
+        i += 1
+    co2 = co2[0]
+    co2_rating = [int(co2[i]) * 2 ** (11 - i) for i in range(12)]
+
+    return sum(oxygen_rating) * sum(co2_rating)
+
 with open("3.txt", "r") as file:
     s = file.read()
 
 numbers = s.split("\n")[:-1]
 print(solve1(numbers))
+print(solve2(numbers))
